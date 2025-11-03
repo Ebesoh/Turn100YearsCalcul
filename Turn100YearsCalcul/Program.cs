b9d0a4f89b1aa6ff9d88a4d9df69855c9e1da1ed
Ebesoh <ebesoh82@gmail.com>
2025-11-03 15:21:36 +0100

    Add project files.

---
+++ Turn100YearsCalcul/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Turn100YearsCalcul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Turn100YearsCalculTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Turn100YearsCalculTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
