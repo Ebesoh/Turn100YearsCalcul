483e7b6e8f8b24b4887d8b091c741297b529940e
Ebesoh <ebesoh82@gmail.com>
2025-11-03 15:22:16 +0100

    Comment added

--- Turn100YearsCalcul/Program.cs
+++ Turn100YearsCalcul/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!"); // Print a greeting message
         }
     }
 }
